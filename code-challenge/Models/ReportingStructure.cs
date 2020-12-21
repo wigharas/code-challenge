8af7f15e4f02fff110491dca0afc960973ead050
walter.igharas <walterigharas@hotmail.com>
2020-12-21 15:30:22 -0500

    initial commit

---
+++ code-challenge/Models/ReportingStructure.cs
@@ -0,0 +1,10 @@
+﻿namespace challenge.Models
+{
+    public class ReportingStructure
+    {
+        
+        public Employee employee { get; set; }
+        
+        public int numberOfReports { get; set; }
+    }
+}
